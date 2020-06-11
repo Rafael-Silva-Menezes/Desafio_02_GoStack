4a8a783d677d98ae69a6bb06377408baf6e2b0d4
Rafael Silva <rafasilva320.rs@gmail.com>
2020-06-11 18:52:32 -0300

    Correção de erro no src/app

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 
 const { uuid } = require("uuidv4");
-const { splice } = require("../../../Aulas/API/src/contacts");
 
 const app = express();
 
